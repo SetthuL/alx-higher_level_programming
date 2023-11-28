3fecc968ca5f4ceee84b7a5bc66e4401dc01bcfa
SetthuL <langesiphosethu@gmail.com>
2023-11-28 13:38:54 -0800

    7-islower.py file update

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    """
-    Returns True if c is lowercase Returns False otherwise
-    """
-    return ord('a') <= ord(c) <= ord('z')
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
+    if ord(c) > 96:
+        return True
+    else:
+        return False
