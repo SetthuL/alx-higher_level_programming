331a2a03fb3789da585742a7fae808fb9df8c18a
SetthuL <langesiphosethu@gmail.com>
2023-11-29 10:43:03 -0800

    8-concat_edges.py file update

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-str = "object-oriented programming with Python"
-print(str[25:] + str[:6] + str[6:25])
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+print(str[39:67] + '\n' + str)
+print(str)
