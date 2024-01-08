20f112b24b9e1eee391b02f6d55a97d9583c6efb
SetthuL <langesiphosethu@gmail.com>
2024-01-08 09:35:38 -0800

    3,4,5 & 6py files created

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Public instance method that raises an Exception
+        with the message area() isNot implemented."""
+        raise Exception("area() is not implemented")
