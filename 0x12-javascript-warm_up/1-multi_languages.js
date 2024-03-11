1038b1035f7dd83084c662903708818b59e59625
SetthuL <langesiphosethu@gmail.com>
2024-03-11 07:06:04 -0700

    1-multi_languages.js file created

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const 1stline = 'C is fun';
+const 2ndline = 'Python is cool';
+const 3rdline = 'JavavaScript is amazing';
+console.log(1stline);
+console.log(2ndline);
+console.log(3rdline);
