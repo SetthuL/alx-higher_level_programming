2d2e55140280bcb04c6cbaaf9ee9b955e05621a4
SetthuL <langesiphosethu@gmail.com>
2024-03-11 09:13:57 -0700

    2-arguments.js update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
 
-const argv.length = process.argv.length(2);
-
-if (args.length === 0)
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else
 {
-	console.log("No argument");
-}
-else if (args.length === 1)
-{
-	 console.log("Argument found");
-}
-else
-{
-	console.log("Arguments found");
+  console.log('Arguments found');
 }
