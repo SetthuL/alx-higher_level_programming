7c24ad98d514c80c1b7d6d3d1a8a810fc311c3be
SetthuL <langesiphosethu@gmail.com>
2024-03-16 07:48:54 -0700

    8-model_state_fetch_first.py modified

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,7 +3,6 @@
 """ A script lists all State objects from the database hbtn_0e_6_usa"""
 
 if __name__ == "__main__":
-
     import sys
     from model_state import Base, State
     from sqlalchemy import create_engine
